d1514aa463a919412686142a9533c86f14a02f2a
kleiver <kleiver1604@gmail.com>
2024-06-13 15:46:04 -0500

    Añadido del responsive y menu de hamburguesa al NavBar

--- virtual-store/src/components/NavButton.tsx
+++ virtual-store/src/components/NavButton.tsx
@@ -1,16 +1,14 @@
+function NavButton({ title, link }) {
+  return (
+    <li className="no-underline inline-block md:block">
+      <a
+        className="text-white font-bold text-center no-underline inline-block w-[150px] md:w-full p-1.5 text-sm md:text-base lg:text-lg hover:underline"
+        href={link}
+      >
+        {title}
+      </a>
+    </li>
+  );
+}
 
-
-function NavButton ({title,link}) {
-
-    return( <>
-<li className="no-underline inline-block ">
-        <a className="text-white font-bold text-center no-underline inline-block w-[150px] p-1.5 text-sm hover:underline " href={link}>
-          {title}
-        </a>
-      </li>
-    </>
-    )
-    }
-
-
-export default NavButton
+export default NavButton;
